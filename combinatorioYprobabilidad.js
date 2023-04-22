cbd19199116b0771d85d9a693b81c7d94934718c
msebaf <msebaf@hotmail.com>
2023-04-22 00:51:43 -0300

    algo de probabilidad

--- combinatorioYprobabilidad.js
+++ combinatorioYprobabilidad.js
@@ -99,4 +99,119 @@ function arreglosSimples() {
     resultadoCombinacionConRepeticion.innerHTML = resultado;
     return resultado;
   }
+
+
+
+  //----------------------arbol de decision
+
+
+
+let tipos = ["Arreglos Simples", "Permutaciones Simples", "Combinaciones Simples", 
+"Arreglos Con Repeticion", "Permutaciones Con Repeticion", "Combinaciones Con Repeticion"];
+
+ function crearEnc(){
+  tipos = ["Arreglos Simples", "Permutaciones Simples", "Combinaciones Simples", 
+"Arreglos Con Repeticion", "Permutaciones Con Repeticion", "Combinaciones Con Repeticion"];
+if(document.getElementById("pp")){
+document.getElementById("pp").remove()
+}
+  if(document.getElementById("divN2")){
+  document.getElementById("divN2").remove();
+  }
+  if(document.getElementById("divN")){
+  document.getElementById("divN").remove();
+  }
+  let encru = document.getElementById("encrucijada");
   
+  let divN= document.createElement('div');
+  divN.id = 'divN';
+  const radio1 = document.createElement('input');
+    radio1.type = 'radio';
+    radio1.name = 'enc';
+    radio1.id = 'ordenSi';
+    radio1.addEventListener('click', function() {
+      crearPregFinal(null, tipos = ["Arreglos Simples", 
+      "Arreglos Con Repeticion"])});
+    const label1 = document.createElement('label');
+    label1.htmlFor = 'ordenSi';
+    label1.textContent = 'Interesa el orden';
+    const radio2 = document.createElement('input');
+    radio2.type = 'radio';
+    radio2.name = 'enc';
+    radio2.id = 'ordenNo';
+    radio2.addEventListener('click', function() {
+      crearPregFinal( null,tipos = ["Arreglos Simples", 
+      "Arreglos Con Repeticion"])});
+    const label2 = document.createElement('label');
+    label2.htmlFor = 'ordenNo';
+    label2.textContent = 'NO interesa el orden';
+    encru.appendChild(divN);
+    divN.appendChild(radio1);
+    divN.appendChild(label1);
+    divN.appendChild(document.createElement('br'));
+    divN.appendChild(radio2);
+    divN.appendChild(label2);
+
+ }
+  
+ function crearPregFinal(boton, tipos){
+  
+  if(document.getElementById("pp")){
+    document.getElementById("pp").remove()
+    }
+  if(boton!=null){
+ if(boton.id == "todos"){
+  tipos = ["Permutaciones Simples" 
+, "Permutaciones Con Repeticion"];
+  if(document.getElementById("divN")){
+  document.getElementById("divN").remove();
+ }
+}}
+if(document.getElementById("divN2")){
+  document.getElementById("divN2").remove();
+}
+let pregFinal = document.getElementById("PregFinal")
+let divN= document.createElement('div');
+divN.id = 'divN2';
+pregFinal.appendChild(divN);
+const radio1 = document.createElement('input');
+    radio1.type = 'radio';
+    radio1.name = 'pf';
+    radio1.id = 'Si';
+    radio1.addEventListener('click',  function() {
+      resultadoSimple( tipos.filter(function(item) {
+        return !item.includes("Repeticion");
+      }))});
+    const label1 = document.createElement('label');
+    label1.htmlFor = 'Si';
+    label1.textContent = 'SI se pueden repetir elementos';
+    const radio2 = document.createElement('input');
+    radio2.type = 'radio';
+    radio2.name = 'pf';
+    radio2.id = 'No';
+    radio1.addEventListener('click', function() {
+      resultadoSimple(tipos.filter(function(item) {
+        return !item.includes("Simples");
+      }))});
+    const label2 = document.createElement('label');
+    label2.htmlFor = 'No';
+    label2.textContent = 'NO se pueden repetir elementos';
+    divN.appendChild(radio1);
+    divN.appendChild(label1);
+    divN.appendChild(document.createElement('br'));
+    divN.appendChild(radio2);
+    divN.appendChild(label2);
+
+}
+
+ 
+
+ function resultadoSimple(tipo){
+  let res = document.getElementById("resultadoEncuesta");
+  
+  p =document.createElement("p");
+  p.id = "pp";
+  p.innerHTML = "Necesitas una " + tipo
+  res.appendChild(p);
+
+ }
---
+++ probabilidad.js
@@ -0,0 +1,34 @@
+
+
+function probClasica() {
+   let fav = parseInt(document.getElementById("favorablesC").value)
+   let total = parseInt(document.getElementById("totalC").value)
+   document.getElementById("resultadoC").innerHTML = (fav/total)
+}
+
+function probCond() {
+    let susN = parseFloat(document.getElementById("sucesoN").value)
+    let sucSus = parseFloat(document.getElementById("sucesoS").value)
+    document.getElementById("resultadoCon").innerHTML = (susN/sucSus)
+ }
+
+ function probIndependientes(){
+    let ind1 = parseFloat(document.getElementById("sucesoAind").value)
+    let ind2 = parseFloat(document.getElementById("sucesoBind").value)
+    document.getElementById("resultadoInd").innerHTML = (ind1 *ind2)
+ }
+
+
+ function probAdiNoD(){
+    let adi1 = parseFloat(document.getElementById("sucesoAadiNoD").value)
+    let adi2 = parseFloat(document.getElementById("sucesoBadiNoD").value)
+    let union = parseFloat(document.getElementById("unionAyB").value)
+    document.getElementById("resultadoAdiNoD").innerHTML = (adi1 + adi2 - (union))
+     
+ }
+ function probAdiD(){
+    let adi1 = parseFloat(document.getElementById("sucesoAadiD").value)
+    let adi2 = parseFloat(document.getElementById("sucesoBadiD").value)
+    document.getElementById("resultadoAdiD").innerHTML = (adi1 + adi2)
+     
+ }
